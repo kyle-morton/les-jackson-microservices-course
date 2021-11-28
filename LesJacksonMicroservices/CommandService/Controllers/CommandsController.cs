3d39ce03797b9593b3a75969ad7f970669d6cfc2
Kyle Morton <kylemortondevelopment@gmail.com>
2021-11-28 17:52:21 -0600

    adding CommandController methods

--- LesJacksonMicroservices/CommandService/Controllers/CommandsController.cs
+++ LesJacksonMicroservices/CommandService/Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandService.Data;
 using CommandService.Dtos;
+using CommandService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandService.Controllers
@@ -30,6 +31,45 @@ namespace CommandService.Controllers
             var commands = _repo.GetCommandsForPlatform(platformId);
 
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId) 
+        {
+            Console.WriteLine("--> Hit GetCommandForPlatform: " + platformId + ", " + commandId);
+
+            if (!_repo.PlatformExists(platformId)) {
+                return NotFound();
+            }
+
+            var command = _repo.GetCommand(platformId, commandId);
+            if (command == null) {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(int platformId, CommandCreateDto dto) 
+        {
+            Console.WriteLine("--> Hit CreateCommand: " + platformId);
+
+            if (!_repo.PlatformExists(platformId)) {
+                return NotFound();
+            }
+
+            var command = _mapper.Map<Command>(dto);
+            _repo.CreateCommand(platformId, command);
+            _repo.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(command);
+            
+            return CreatedAtRoute(
+                nameof(GetCommandForPlatform),
+                new { platformId = platformId, commandId = commandReadDto.Id },
+                commandReadDto
+            );
         }
     }
 }
